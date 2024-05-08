e4ee8c62694286dc2600947316c215fe6bc3c48f
Felipe Travi <felipe.travi@g.globo>
2024-05-08 11:10:52 -0300

    add contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Card, CardContent, Box } from '@mui/material';
 
-function Home() {
+function Contact() {
   return (
     <Container maxWidth="md" sx={{ textAlign: 'center', marginTop: '50px' }}>
-      <Typography variant="h4" gutterBottom>
-        Bem-vindo ao site de organização de doações para vítimas da enchente no Rio Grande do Sul!
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Nosso objetivo é facilitar a integração dos voluntários e ajudar na organização das doações para as vítimas da enchente.
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        É importante ressaltar que não temos responsabilidade sobre o conteúdo das doações, uma vez que o cadastro é público.
-      </Typography>
-      <Button variant="contained" color="primary" href="/locaiscoleta">
-        Onde doar
-      </Button>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h4" gutterBottom>
+            Entre em contato conosco
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Estamos aqui para ajudar! Se você tiver alguma dúvida, sugestão, ou desejar fazer alguma contribuição para melhorar nosso serviço e ajudar as vítimas da enchente, por favor, não hesite em nos contatar, nosso email é administrador@doacoesrs.com.br
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+            <img src="https://www.estado.rs.gov.br/upload/recortes/201707/20075647_1210628_GDO.jpg" alt="Bandeira do Rio Grande do Sul" style={{ maxWidth: '200px', height: 'auto' }} />
+          </Box>
+          <Typography variant="body2" sx={{ mt: 1 }}>
+            Estamos constantemente trabalhando para melhorar nossos serviços e sua opinião é muito importante para nós. Agradecemos antecipadamente por entrar em contato!
+          </Typography>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
 
-export default Home;
+export default Contact;
